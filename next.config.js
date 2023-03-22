2db840a8d17a3dcfaf4d051e6ee304e4dd5cb609
cm-ayf <cm.ayf2734@gmail.com>
2023-03-22 19:03:48 +0900

    /api/users/me NetworkOnly

--- next.config.js
+++ next.config.js
@@ -4,6 +4,13 @@ const nextPWA = require("next-pwa");
 module.exports = nextPWA({
   dest: "public",
   disable: process.env.NODE_ENV !== "production",
+  runtimeCaching: [
+    {
+      urlPattern: "/api/users/me",
+      method: "GET",
+      handler: "NetworkOnly",
+    },
+  ],
 })({
   reactStrictMode: true,
 });
