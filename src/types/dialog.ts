fedb604da5066631ca18f9aa5c469c2607082de0
cm-ayf <cm.ayf2734@gmail.com>
2023-04-15 02:38:02 +0900

    dialogs accept button color

--- src/types/dialog.ts
+++ src/types/dialog.ts
@@ -1,8 +1,12 @@
 import type { Static, TSchema } from "@sinclair/typebox";
+import type { ButtonTypeMap } from "@mui/material/Button";
+
+type Color = ButtonTypeMap["props"]["color"];
 
 interface PlainDialogButton {
   label: string;
   needsValidation?: false;
+  color?: Color;
   needsUpdate?: boolean;
   onClick: () => void | Promise<void>;
 }
@@ -10,6 +14,7 @@ interface PlainDialogButton {
 interface ValidatedDialogButton<T extends TSchema> {
   label: string;
   needsValidation: true;
+  color?: Color;
   needsUpdate?: boolean;
   onClick: (body: Static<T>) => void | Promise<void>;
 }
