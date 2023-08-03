dced9a1fcd1e2deae8ca8b6f4109734c52003895
cm-ayf <cm.ayf2734@gmail.com>
2023-08-03 13:54:48 +0900

    extend OAuth2Error

--- src/shared/error.ts
+++ src/shared/error.ts
@@ -1,3 +1,27 @@
+import { Static, Type } from "@sinclair/typebox";
+import { TypeCompiler } from "@sinclair/typebox/compiler";
+
+const OAuth2ErrorCode = Type.Enum({
+  unknown_guild: "unknown_guild",
+  invalid_credentials: "invalid_credentials",
+  invalid_request: "invalid_request",
+  server_error: "server_error",
+} as const);
+
+type OAuth2ErrorCode = Static<typeof OAuth2ErrorCode>;
+
+const typeCheckCode = TypeCompiler.Compile(OAuth2ErrorCode);
+
+const OAuth2ErrorJson = Type.Object({
+  error: Type.Optional(Type.String()),
+  error_description: Type.Optional(Type.String()),
+  code: OAuth2ErrorCode,
+});
+
+type OAuth2ErrorJson = Static<typeof OAuth2ErrorJson>;
+
+const typeCheckJson = TypeCompiler.Compile(OAuth2ErrorJson);
+
 export class OAuth2Error extends Error {
   static fromError(e: unknown) {
     if (e instanceof this) {
@@ -7,12 +31,26 @@ export class OAuth2Error extends Error {
     }
   }
 
+  static fromSearch(search: URLSearchParams) {
+    const description = search.get("error_description") ?? undefined;
+    const code = search.get("code");
+    if (typeCheckCode.Check(code)) {
+      return new this(code, description);
+    } else {
+      return new this("server_error", description);
+    }
+  }
+
+  static fromJSON(e: unknown) {
+    if (typeCheckJson.Check(e)) {
+      return new this(e.code, e.error_description);
+    } else {
+      return new this("server_error", undefined, { cause: e });
+    }
+  }
+
   constructor(
-    public code:
-      | "unknown_guild"
-      | "invalid_credentials"
-      | "invalid_request"
-      | "server_error",
+    public code: OAuth2ErrorCode,
     public description?: string,
     options?: ErrorOptions
   ) {
@@ -23,7 +61,7 @@ export class OAuth2Error extends Error {
     return this.code === "server_error" ? 500 : 400;
   }
 
-  toJSON() {
+  toJSON(): OAuth2ErrorJson {
     const error = {
       invalid_request: "invalid_request",
       invalid_credentials: "invalid_grant",
@@ -31,25 +69,24 @@ export class OAuth2Error extends Error {
       server_error: undefined,
     }[this.code];
     return {
-      error,
-      error_description: this.description,
+      ...(error && { error }),
+      ...(this.description && { error_description: this.description }),
       code: this.code,
     };
   }
 
   toRedirectURL() {
-    const searchParams = new URLSearchParams();
     const error = {
       invalid_request: "invalid_request",
       invalid_credentials: "access_denied",
       unknown_guild: "access_denied",
       server_error: "server_error",
     }[this.code];
-    searchParams.append("error", error);
-    if (this.description) {
-      searchParams.append("error_description", this.description);
-    }
-    searchParams.append("code", this.code);
+    const searchParams = new URLSearchParams({
+      error,
+      ...(this.description && { error_description: this.description }),
+      code: this.code,
+    });
     return `/?${searchParams.toString()}`;
   }
 }
