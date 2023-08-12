dbc05613669c4bc0f40530bab1311b518e985136
cm-ayf <cm.ayf2734@gmail.com>
2023-08-12 15:48:09 +0900

    media-aware top in receipts

---
+++ src/app/[eventcode]/receipts/Top.tsx
@@ -0,0 +1,56 @@
+import { useMediaQuery } from "@mui/material";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Tab from "@mui/material/Tab";
+import MuiTabs from "@mui/material/Tabs";
+import ReloadButton from "./ReloadButton";
+import { DeleteButton } from "./Table";
+import { SyncButton } from "@/components/SyncButton";
+
+export default function Top({
+  eventcode,
+  tab,
+  setTab,
+}: {
+  eventcode: string;
+  tab: string;
+  setTab: (tab: string) => void;
+}) {
+  const small = useMediaQuery("(max-width: 600px)");
+  return (
+    <Container
+      sx={{ display: "flex", flexDirection: small ? "column-reverse" : "row" }}
+    >
+      <Tabs tab={tab} setTab={setTab} />
+      <Box sx={{ flex: 1 }} />
+      <Buttons eventcode={eventcode} showDelete={tab === "table"} />
+    </Container>
+  );
+}
+
+function Tabs({ tab, setTab }: { tab: string; setTab: (tab: string) => void }) {
+  return (
+    <MuiTabs value={tab} onChange={(_, value) => setTab(value)}>
+      <Tab label="概要" value="summary" />
+      <Tab label="表" value="table" />
+      <Tab label="出力" value="export" />
+    </MuiTabs>
+  );
+}
+
+function Buttons({
+  eventcode,
+  showDelete,
+}: {
+  eventcode: string;
+  showDelete: boolean;
+}) {
+  return (
+    <Box sx={{ display: "flex", p: 1, gap: 1 }}>
+      <Box sx={{ flex: 1 }} />
+      {showDelete && <DeleteButton eventcode={eventcode} variant="contained" />}
+      <SyncButton eventcode={eventcode} variant="contained" />
+      <ReloadButton eventcode={eventcode} variant="contained" />
+    </Box>
+  );
+}
--- src/app/[eventcode]/receipts/page.tsx
+++ src/app/[eventcode]/receipts/page.tsx
@@ -2,16 +2,12 @@
 
 import TabContext from "@mui/lab/TabContext";
 import TabPanel from "@mui/lab/TabPanel";
-import Box from "@mui/material/Box";
-import Tab from "@mui/material/Tab";
-import Tabs from "@mui/material/Tabs";
 import { useState } from "react";
 import Export from "./Export";
-import ReloadButton from "./ReloadButton";
 import Summary from "./Summary";
-import Table, { DeleteButton, SelectedProvider } from "./Table";
+import Table, { SelectedProvider } from "./Table";
+import Top from "./Top";
 import Layout from "@/components/Layout";
-import { SyncButton } from "@/components/SyncButton";
 import { DBStateProvider } from "@/hooks/DBState";
 import { useTitle } from "@/hooks/swr";
 
@@ -34,51 +30,19 @@ export default function Receipts({
           docs="receipts"
           top={<Top eventcode={eventcode} tab={tab} setTab={setTab} />}
         >
-          <Panels eventcode={eventcode} tab={tab} />
+          <TabContext value={tab}>
+            <TabPanel value="summary" sx={{ p: 0 }}>
+              <Summary eventcode={eventcode} />
+            </TabPanel>
+            <TabPanel value="table" sx={{ p: 0 }}>
+              <Table eventcode={eventcode} />
+            </TabPanel>
+            <TabPanel value="export" sx={{ p: 0 }}>
+              <Export eventcode={eventcode} />
+            </TabPanel>
+          </TabContext>
         </Layout>
       </SelectedProvider>
     </DBStateProvider>
-  );
-}
-
-function Top({
-  eventcode,
-  tab,
-  setTab,
-}: {
-  eventcode: string;
-  tab: string;
-  setTab: (tab: string) => void;
-}) {
-  return (
-    <Box sx={{ display: "flex", flexDirection: "row" }}>
-      <Tabs value={tab} onChange={(_, value) => setTab(value)}>
-        <Tab label="概要" value="summary" />
-        <Tab label="表" value="table" />
-        <Tab label="出力" value="export" />
-      </Tabs>
-      <Box sx={{ flex: 1 }} />
-      {tab === "table" && (
-        <DeleteButton eventcode={eventcode} variant="contained" sx={{ m: 1 }} />
-      )}
-      <SyncButton eventcode={eventcode} variant="contained" sx={{ m: 1 }} />
-      <ReloadButton eventcode={eventcode} variant="contained" sx={{ m: 1 }} />
-    </Box>
-  );
-}
-
-function Panels({ eventcode, tab }: { eventcode: string; tab: string }) {
-  return (
-    <TabContext value={tab}>
-      <TabPanel value="summary" sx={{ p: 0 }}>
-        <Summary eventcode={eventcode} />
-      </TabPanel>
-      <TabPanel value="table" sx={{ p: 0 }}>
-        <Table eventcode={eventcode} />
-      </TabPanel>
-      <TabPanel value="export" sx={{ p: 0 }}>
-        <Export eventcode={eventcode} />
-      </TabPanel>
-    </TabContext>
   );
 }
